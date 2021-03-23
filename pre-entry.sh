31c7dd4a312de490ed6cbaa69421c4366039c3ae
Waldemar Tomme <waldetomme@gmail.com>
2021-03-23 19:05:30 +0100

    T_T

--- pre-entry.sh
+++ pre-entry.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sudo groupmod -g $1 docker
 sudo usermod -a -G docker jenkins
-sudo su -c '/sbin/tini -- /usr/local/bin/jenkins.sh' - jenkins
+# sudo su -c '/sbin/tini -- /usr/local/bin/jenkins.sh' - jenkins
+/sbin/tini -- /usr/local/bin/jenkins.sh
